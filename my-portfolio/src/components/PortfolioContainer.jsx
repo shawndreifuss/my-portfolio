acc6573cff3d56a908866c60f476bf6643bbe361
Shawn Dreifuss <shawndreifuss@icloud.com>
2023-11-08 15:56:15 -0800

    added portfolio containre with renderpage function

--- my-portfolio/src/components/PortfolioContainer.jsx
+++ my-portfolio/src/components/PortfolioContainer.jsx
@@ -9,4 +9,32 @@ import Contact from './pages/Contact';
 export default function PortfolioContainer() {
     //set deafult page to home 
     const [currentPage, setCurrentPage] = useState('Home');
-}
+
+    // Method to check currentPage and return component to render
+    const renderPage = () => {
+        if (currentPage === 'Home') {
+            return <Home />;
+        }
+        if (currentPage === 'Home') {
+            return <About />;
+        }
+        if (currentPage === 'Home') {
+            return <Work />;
+        }
+        if (currentPage === 'Home') {
+            return <Skills />;
+        }
+        if (currentPage === 'Home') {
+            return <Contact />;
+        }
+    };
+
+    const handlePageChange = (page) => setCurrentPage(page);
+
+    return (
+        <body>
+          <Navbar currentPage={currentPage} handlePageChange={handlePageChange} />
+          <div>{renderPage()}</div>
+        </body>
+            )
+};
