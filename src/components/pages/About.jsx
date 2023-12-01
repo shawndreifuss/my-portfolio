505c8496efab9d6c657df802a6003419ca4796e8
Shawn Dreifuss <shawndreifuss@icloud.com>
2023-11-30 18:02:05 -0800

    about page restructure

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -84,7 +84,7 @@ export default function About({ currentPage, handlePageChange }) {
           </div>
         </div>
       </div>
-      <div className="colorlib-skills" data-section="skills">
+      {/* <div className="colorlib-skills" data-section="skills">
 				<div className="colorlib-narrow-content">
 					<div className="row">
 						<div className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
@@ -164,7 +164,7 @@ export default function About({ currentPage, handlePageChange }) {
 					</div>
 				</div>
 			</div>
-      
+       */}
       
     </section>
 );
